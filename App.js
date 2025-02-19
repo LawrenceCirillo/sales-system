193b88330260026ec7f4fdd2d8fb7c9169024e35
LawrenceCirillo <ljcirills@gmail.com>
2025-02-19 12:20:08 -0500

    Initial commit with existing project

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* Screens will be added here */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+} 
